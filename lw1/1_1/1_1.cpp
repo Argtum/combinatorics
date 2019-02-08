381a210caa80200213f9e6bc336f65ac3505fce5
Alexey Serebryakov <silwar40k@gmail.com>
2019-02-09 02:49:29 +0300

    набил код для 1.1 - генерацию отрабатывает

---
+++ lw1/1_1/1_1.cpp
@@ -0,0 +1,24 @@
+﻿#include "pch.h"
+#include <algorithm>
+#include <boost/timer.hpp>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+int main()
+{
+	std::vector<size_t> v = { 7, 1, 12, 5, 9, 20, 15, 2, 6, 13 };
+	boost::timer t;
+
+	t.restart();
+	std::sort(v.begin(), v.end());
+	do
+	{
+		//copy(v.begin(), v.end(), std::ostream_iterator<size_t>(std::cout, " "));
+		//std::cout << std::endl;
+	} while (std::next_permutation(v.begin(), v.end()));
+	double duration = t.elapsed();
+	std::cout << duration << std::endl;
+
+	return 0;
+}
