d0345c48edea89669cd470f40d49af3918f25154
Alexey Serebryakov <silwar40k@gmail.com>
2019-02-10 04:20:27 +0300

    сделал 1.3

---
+++ lw1/1_3/1_3.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <random>
+#include <iterator>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	random_device rd;
+	mt19937 g(rd());
+	vector<size_t> v = { 7, 1, 9, 4, 5, 0, 2, 3, 8, 6 };
+	char again;
+
+	cout << "До перемешивания:    ";
+	copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+	do
+	{
+		cout << endl << "После перемешивания: ";
+		shuffle(v.begin(), v.end(), g);
+		copy(v.begin(), v.end(), ostream_iterator<int>(cout, " "));
+		do
+		{
+			cout << endl << "Ещё раз?(y/n): ";
+			cin >> again;
+		} while (again != 'y' && again != 'n');
+	} while (again == 'y');
+
+	return 0;
+}
