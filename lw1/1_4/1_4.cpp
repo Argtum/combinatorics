237bda49cf8c94c48074d35ae0322c0c9d744ea2
Alexey Serebryakov <silwar40k@gmail.com>
2019-02-16 19:43:08 +0300

    сделал 4-ю лабу с замерами

---
+++ lw1/1_4/1_4.cpp
@@ -0,0 +1,44 @@
+#include "pch.h"
+#include <algorithm>
+#include <boost/timer.hpp>
+#include <iostream>
+#include <iterator>
+#include <vector>
+
+const int N = 100; // общее кол-во элементов
+const int K = 5;  // количество сочетающихся элементов 
+
+int main()
+{
+	boost::timer t;
+	std::vector<size_t> a(K + 1);
+	a[0] = -1;
+	int m = 1;
+	for (int i = 1; i <= K; i++)
+	{
+		a[i] = i;
+	}
+
+	t.restart();
+	while (m != 0)
+	{
+		//copy(a.begin() + 1, a.end(), std::ostream_iterator<size_t>(std::cout, " "));
+		//std::cout << std::endl;
+		m = K;
+		
+		while (a[m] == N - K + m)
+		{
+			m--;
+		}
+		a[m]++;
+		for (int i = m + 1; i <= K; i++)
+		{
+			a[i] = a[i - 1] + 1;
+		}
+	}
+
+	double duration = t.elapsed();
+	std::cout << duration << std::endl;
+
+	return 0;
+}
